6c2eef48419496a1e0f5598640ffa83ee1658740
Alex Groat <alex.groat.95@gmail.com>
2022-04-26 11:01:47 +0930

    install and using jetstream:

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Welcome!</div>
+</template>
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  components: {},
+
+  props: {
+    canLogin: Boolean,
+    canRegister: Boolean,
+  },
+});
+</script>
+
+
