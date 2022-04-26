05b81f3520147f42317b2fb60dfef813388f293f
Alex Groat <alex.groat.95@gmail.com>
2022-04-26 11:58:41 +0930

    using inertiajs and vuejs and creating a custom section component

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -1,6 +1,34 @@
 <template>
+  <Head title="Welcome" />
+
+  <Section
+    class="grid grid-cols-2 text-right bg-gray-800 text-gray-300"
+  >
+    <div>
+      <jet-application-mark class="h-12 mx-auto"></jet-application-mark>
+    </div>
+    <div v-if="canLogin">
+      <Link
+        v-if="$page.props.user"
+        :href="route('dashboard')"
+        class="text-sm underline"
+        >Dashboard
+      </Link>
+
+      <template v-else>
+        <Link :href="route('login')" class="text-base underline">Log in</Link>
+
+        <Link
+          v-if="canRegister"
+          :href="route('register')"
+          class="text-base underline place-self-end ml-4"
+          >Register</Link
+        >
+      </template>
+    </div>
+  </Section>
   <div>
-    <section class="bg-gray-800 pt-16 h-screen px-72">
+    <Section class="bg-gray-800 pt-16 h-screen ">
       <div class="h-2/3 flex flex-wrap content-between pb-36">
         <p
           class="
@@ -16,27 +44,39 @@
           Hey! This is Alex. I'm a software developer.
         </p>
       </div>
-    </section>
+    </Section>
 
-    <section class="bg-gray-200 text-gray-800 px-72 py-10 h-screen">
+    <Section class="bg-gray-200 text-gray-800 h-screen">
       <h2 class="text-6xl font-bold pt-3">Skills</h2>
-    </section>
+    </Section>
 
-    <section class="bg-gray-600 text-gray-200 px-72 py-10 h-screen">
+    <Section class="bg-gray-600 text-gray-200 h-screen">
       <h2 class="text-6xl font-bold pt-3">Projects</h2>
-    </section>
+    </Section>
 
-    <section class="flex justify-between py-10 px-72 bg-gray-800 text-gray-300 text-xl">
+    <Section
+      class="flex justify-between bg-gray-800 text-gray-300 text-xl"
+    >
       <p>&copy; AlexGroat. All Rights Reserved.</p>
-      <div class="flex justify-evenly items-center">Github Twitter StackOverflow</div>
-    </section>
+      <div class="flex justify-evenly items-center">
+        Github Twitter StackOverflow
+      </div>
+    </Section>
   </div>
 </template>
 <script>
 import { defineComponent } from "vue";
+import { Head, Link } from "@inertiajs/inertia-vue3";
+import JetApplicationMark from '@/Jetstream/ApplicationMark';
+import Section from '../Components/Section.vue';
 
 export default defineComponent({
-  components: {},
+  components: {
+    Head,
+    Link,
+    Section,
+    JetApplicationMark
+  },
 
   props: {
     canLogin: Boolean,
