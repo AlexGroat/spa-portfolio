468624e5408a51e0ba1c3df88f44613b0fc25ed0
Alex Groat <alex.groat.95@gmail.com>
2022-04-27 09:53:34 +0930

    show the project list in admin panel episode 25

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Project;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class ProjectController extends Controller
 {
@@ -14,7 +15,9 @@ class ProjectController extends Controller
      */
     public function index()
     {
-        //
+        return Inertia::render('Projects/All', [
+            'projects' => Project::all()
+        ]);
     }
 
     /**
