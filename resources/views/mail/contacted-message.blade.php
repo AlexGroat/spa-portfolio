697f65775b2083e0ab7bac98d0a1368680005bd0
Alex Groat <alex.groat.95@gmail.com>
2022-04-28 16:17:59 +0930

    update

--- resources/views/mail/contacted-message.blade.php
+++ resources/views/mail/contacted-message.blade.php
@@ -1,7 +1,7 @@
 @component('mail::message')
 # Hi there
 
-Someone has contacted you with this messge:
+Someone has contacted you with this message:
 "{{ $message }}"
 
 Maybe, you want to get in touch with them, using this shared email: "{{ $email }}}"
